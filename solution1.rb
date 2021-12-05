c6649aab8c7937113da2442af6778c4e23093bbc
Andy McVitty <andy.mcvitty@gmail.com>
2021-12-05 16:56:15 +0000

    Day 4

--- solution1.rb
+++ solution1.rb
@@ -1,40 +1,85 @@
-data = $stdin.read.split("\n")
+realdata = $stdin.read.split("\n")
 
-counts = []
-data[0].split("").size.times { counts.push(0) }
-data.each do |line|
-  nums = line.split("")
-  nums.each_with_index { |num, index|
-    counts[index] += num.to_i
-  }
+numbers = realdata[0].split(",").map(&:to_i)
+
+def readline(data, index)
+  data[index + 0].strip.split(/ +/).map(&:to_i)
 end
 
-print "epsilon: "
-epsilon = 0
-counts.each_with_index { |num, index|
-  digit = (if num > data.size / 2
-    0
-  else
-    1
-  end)
-  epsilon = epsilon * 2 + digit
-  print digit
-}
-puts
-puts epsilon
+def readboard(data, index)
+  board = []
+  board.push readline(data, index)
+  board.push readline(data, index + 1)
+  board.push readline(data, index + 2)
+  board.push readline(data, index + 3)
+  board.push readline(data, index + 4)
+end
 
-print "gamma: "
-gamma = 0
-counts.each_with_index { |num, index|
-  digit = (if num < data.size / 2
-    0
-  else
-    1
-  end)
-  gamma = gamma * 2 + digit
-  print digit
-}
-puts
-puts gamma
+def gen_checkboard
+  cb = []
+  5.times {
+    cba = []
+    5.times { cba.push(false) }
+    cb.push cba
+  }
+  cb
+end
 
-puts "Power consumption: #{gamma * epsilon}"
+Board = Struct.new(:numbers, :checks) do
+  def print
+    puts (0..4).map { |r|
+      (0..4).map { |c|
+        sprintf("%2d", numbers[r][c]) << (checks[r][c] ? "x" : " ")
+      }.join(", ")
+    }.join("\n")
+  end
+
+  def mark(num)
+    (0..4).map { |r|
+      (0..4).map { |c|
+        checks[r][c] = true if numbers[r][c] == num
+      }
+    }
+  end
+
+  def winning
+    winning = false
+    (0..4).each { |r|
+      winning = true if checks[r].all?
+    }
+    (0..4).each { |c|
+      winning = true if (0..4).map { |r| checks[r][c] }.all?
+    }
+    winning
+  end
+
+  def sum_unmarked
+    sum = 0
+    (0..4).each { |r|
+      (0..4).each { |c|
+        sum += numbers[r][c] unless checks[r][c]
+      }
+    }
+    sum
+  end
+end
+
+boards = []
+line = 2
+while line < realdata.size
+  boards.push Board.new(readboard(realdata, line), gen_checkboard)
+  line += 6
+end
+
+numbers.each do |num|
+  boards.each_with_index do |board, index|
+    board.mark(num)
+    if board.winning
+      puts "Board: #{index}"
+      puts board.sum_unmarked
+      puts "number: #{num}"
+      puts board.sum_unmarked * num
+      exit 0
+    end
+  end
+end
--- solution2.rb
+++ solution2.rb
@@ -1,66 +1,98 @@
-data = $stdin.read.split("\n").map do |line|
-  line.split("").map(&:to_i)
+realdata = $stdin.read.split("\n")
+
+numbers = realdata[0].split(",").map(&:to_i)
+
+def readline(data, index)
+  data[index + 0].strip.split(/ +/).map(&:to_i)
 end
 
-def count(lines, index)
-  c = [0, 0]
-  lines.each { |arr|
-    c[arr[index]] += 1
+def readboard(data, index)
+  board = []
+  board.push readline(data, index)
+  board.push readline(data, index + 1)
+  board.push readline(data, index + 2)
+  board.push readline(data, index + 3)
+  board.push readline(data, index + 4)
+end
+
+def gen_checkboard
+  cb = []
+  5.times {
+    cba = []
+    5.times { cba.push(false) }
+    cb.push cba
   }
-  c
+  cb
 end
 
-def filter_by_pos(lines, index, filter)
-  lines.filter { |x| x[index] == filter }
+Board = Struct.new(:numbers, :checks, :winturn) do
+  def print
+    puts (0..4).map { |r|
+      (0..4).map { |c|
+        sprintf("%2d", numbers[r][c]) << (checks[r][c] ? "x" : " ")
+      }.join(", ")
+    }.join("\n")
+  end
+
+  def mark(num)
+    (0..4).map { |r|
+      (0..4).map { |c|
+        checks[r][c] = true if numbers[r][c] == num
+      }
+    }
+  end
+
+  def winning
+    winning = false
+    (0..4).each { |r|
+      winning = true if checks[r].all?
+    }
+    (0..4).each { |c|
+      winning = true if (0..4).map { |r| checks[r][c] }.all?
+    }
+    winning
+  end
+
+  def sum_unmarked
+    sum = 0
+    (0..4).each { |r|
+      (0..4).each { |c|
+        sum += numbers[r][c] unless checks[r][c]
+      }
+    }
+    sum
+  end
 end
 
-def ogr(lines, index)
-  if lines.size == 1
-    lines[0]
-  else
-    (a, b) = count(lines, index)
-    if b >= a
-      ogr(filter_by_pos(lines, index, 1), index + 1)
-    else
-      ogr(filter_by_pos(lines, index, 0), index + 1)
+boards = []
+line = 2
+while line < realdata.size
+  boards.push Board.new(readboard(realdata, line), gen_checkboard)
+  line += 6
+end
+
+boards.each_with_index do |board, index|
+  numbers.each_with_index do |num, numcount|
+    if !board.winning
+      board.mark(num)
+      if board.winning
+        board.winturn = numcount
+        puts "Board: #{index}"
+        board.print
+        puts board.sum_unmarked
+        puts "number: #{num}, num_count: #{numcount}"
+        puts board.sum_unmarked * num
+      end
     end
   end
 end
 
-def csr(lines, index)
-  if lines.size == 1
-    lines[0]
-  else
-    (a, b) = count(lines, index)
-    if a <= b
-      csr(filter_by_pos(lines, index, 0), index + 1)
-    else
-      csr(filter_by_pos(lines, index, 1), index + 1)
-    end
-  end
+boards.each_with_index do |board, index|
+  puts "Index: #{index}, win turn: #{board.winturn}"
 end
 
-def arr_to_i(arr)
-  i = 0
-  arr.each { |digit| i = i * 2 + digit }
-  i
-end
+maxboard = boards.max_by { |b| b.winturn }
+puts maxboard.sum_unmarked * numbers[maxboard.winturn]
 
-o = ogr(data, 0)
-c = csr(data, 0)
-puts o.to_s
-puts arr_to_i o
-puts c.to_s
-puts arr_to_i c
-
-puts ((arr_to_i o) * (arr_to_i c))
-# e = ((0..4).to_a.map { |i|
-#   (a, b) = count(lines, i)
-#   if a > b
-#     0
-#   else
-#     1
-#   end
-# })
-# puts e.to_s
-# puts arr_to_i(e)
+minboard = boards.min_by { |b| b.winturn }
+puts minboard.sum_unmarked * numbers[minboard.winturn]
