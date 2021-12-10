837b02f534e326ad873b60e205974931ea2421b5
Andy McVitty <andy.mcvitty@gmail.com>
2021-12-10 19:49:13 +0000

    Day 10 part 1

--- lib.rb
+++ lib.rb
@@ -1,49 +1,37 @@
-require "set"
-Point = Struct.new(:r, :c, :max_row, :max_col) do
-  def adjacent_points
-    p = []
-    p << Point.new(r - 1, c, max_row, max_col) if r > 0
-    p << Point.new(r + 1, c, max_row, max_col) if r < max_row
-    p << Point.new(r, c - 1, max_row, max_col) if c > 0
-    p << Point.new(r, c + 1, max_row, max_col) if c < max_col
-    p
-  end
-
-  def inspect
-    "(#{r}, #{c})"
-  end
-end
-
-def basin(low_point, grid)
-  explored = Set[]
-  unexplored = Set[low_point]
-  while unexplored.size > 0
-    p = unexplored.to_a[0]
-    p.adjacent_points.each do |a|
-      if grid[a.r][a.c] >= grid[a.r][a.c] &&
-         grid[a.r][a.c] != 9 &&
-         !explored.include?(a) &&
-         !unexplored.include?(a)
-        unexplored << a
-      end
-      unexplored.delete p
-      explored.add p
-    end
-  end
-  explored
-end
-
-def print_grid_with_points(grid, points)
-  max_row = grid.size - 1
-  max_col = grid[0].size - 1
-  (0..max_row).each do |r|
-    (0..max_col).each do |c|
-      if points.include?(Point.new(r, c, max_row, max_col))
-        print grid[r][c], " "
+def parse_line(line)
+  stack = []
+  line.chars.each do |c|
+    case c
+    when "(", "[", "{", "<"
+      stack.push c
+    when ")", "}", "]", ">"
+      if pair(stack.last) == c
+        stack.pop
       else
-        print "  "
+        puts "Expected #{pair(stack.last)}, but found #{c} instead."
+        return score(c)
       end
     end
-    puts
   end
+  return 0
+end
+
+def pair(c)
+  case c
+  when "("
+    ")"
+  when "["
+    "]"
+  when "{"
+    "}"
+  when "<"
+    ">"
+  end
+end
+
+def score(c)
+  { ")" => 3,
+    "]" => 57,
+    "}" => 1197,
+    ">" => 25137 }[c]
 end
--- main.rb
+++ main.rb
@@ -1,38 +1,7 @@
 require_relative "lib.rb"
 lines = $stdin.read.split("\n")
-grid = lines.map { |line|
-  line.chars.map(&:to_i)
-}
-
-max_row = grid.size - 1
-max_col = grid[0].size - 1
-risk = 0
-low_points = []
-(0..max_row).each do |r|
-  (0..max_col).each do |c|
-    lowest = true
-    me = grid[r][c]
-    lowest = false if r > 0 && grid[r - 1][c] <= me
-    lowest = false if r < max_row && grid[r + 1][c] <= me
-    lowest = false if c > 0 && grid[r][c - 1] <= me
-    lowest = false if c < max_col && grid[r][c + 1] <= me
-    if lowest
-      risk += 1 + me
-      low_points << Point.new(r, c, max_row, max_col)
-    end
-  end
-end
-print_grid_with_points(grid, low_points)
-puts "-----"
-puts "Part 1: "
-puts "Risk: #{risk}"
-puts low_points.to_s
-puts "-----"
-basin_sizes = low_points.map do |low_point|
-  b = basin(low_point, grid)
-  puts "Basin size: #{b.size}"
-  print_grid_with_points(grid, b)
-  puts "-----"
-  b.size
-end
-puts basin_sizes.sort.reverse.take(3).reduce(&:*)
+puts lines.map { |line|
+  s = parse_line line
+  puts s
+  s
+}.sum
