f56ef9d5fe6f7ae116d7fd54170d4072c44ec19c
Andy McVitty <andy.mcvitty@gmail.com>
2021-12-20 09:31:24 +0000

    Day 19 part 1

--- lib.rb
+++ lib.rb
@@ -1,196 +1,136 @@
-class PairParser
-  attr_accessor :str, :i
+require "matrix"
+require "set"
 
-  def initialize(str, i = 0)
-    self.str = str
-    self.i = 0
-  end
-
-  def parse
-    if peek == "["
-      @i += 1
-      l = parse
-      expect(",")
-      r = parse
-      expect("]")
-      return Node.new(l, r)
+def read_scanners
+  lines = $stdin.read.split("\n")
+  scanners = []
+  scanner = nil
+  lines.each do |line|
+    if line.match(/scanner/)
+      scanners << scanner if !scanner.nil?
+      scanner = []
+    elsif line == ""
     else
-      val = read_val
-      return Leaf.new(val.to_i)
+      x, y, z = line.split(",").map(&:to_i)
+      scanner << Matrix.column_vector([x, y, z])
+    end
+  end
+  scanners << scanner
+
+  scanners
+end
+
+#Matrix[ [25, 93], [-1, 66] ]
+
+# https://en.wikipedia.org/wiki/Rotation_matrix#In_three_dimensions
+def rx(r)
+  Matrix[
+    [1, 0, 0],
+    [0, cos(r), -sin(r)],
+    [0, sin(r), cos(r)],
+  ]
+end
+
+def ry(r)
+  Matrix[
+    [cos(r), 0, sin(r)],
+    [0, 1, 0],
+    [-sin(r), 0, cos(r)],
+  ]
+end
+
+def rz(r)
+  Matrix[
+    [cos(r), -sin(r), 0],
+    [sin(r), cos(r), 0],
+    [0, 0, 1]
+  ]
+end
+
+def sort_points(points)
+  points.sort { |a, b|
+    a[0, 0] <=> b[0, 0] ||
+    a[1, 0] <=> b[1, 0] ||
+    a[2, 0] <=> b[2, 0]
+  }
+end
+
+def puts_points(points)
+  sort_points(points).each { |a|
+    puts "#{a[0, 0]},#{a[1, 0]},#{a[2, 0]}"
+  }
+end
+
+def get_rotations
+  angles = [0, Math::PI / 2, Math::PI, 3 * Math::PI / 2]
+  xs = angles.map { |r| rx(r) }
+  ys = angles.map { |r| ry(r) }
+  zs = angles.map { |r| rz(r) }
+
+  rots = Set[]
+
+  xs.each do |x|
+    ys.each do |y|
+      zs.each do |z|
+        rots << (x * y * z)
+      end
     end
   end
 
-  def peek
-    str[i]
-  end
+  rots.to_a
+end
 
-  def read_char
-    v = str[i]
-    @i += 1
-    v
-  end
+# pre-calced version of the above
+def get_rotations2
+  [Matrix[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+   Matrix[[0, -1, 0], [1, 0, 0], [0, 0, 1]],
+   Matrix[[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
+   Matrix[[0, 1, 0], [-1, 0, 0], [0, 0, 1]],
+   Matrix[[0, 0, 1], [0, 1, 0], [-1, 0, 0]],
+   Matrix[[0, 0, 1], [1, 0, 0], [0, 1, 0]],
+   Matrix[[0, 0, 1], [0, -1, 0], [1, 0, 0]],
+   Matrix[[0, 0, 1], [-1, 0, 0], [0, -1, 0]],
+   Matrix[[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
+   Matrix[[0, 1, 0], [1, 0, 0], [0, 0, -1]],
+   Matrix[[1, 0, 0], [0, -1, 0], [0, 0, -1]],
+   Matrix[[0, -1, 0], [-1, 0, 0], [0, 0, -1]],
+   Matrix[[0, 0, -1], [0, 1, 0], [1, 0, 0]],
+   Matrix[[0, 0, -1], [1, 0, 0], [0, -1, 0]],
+   Matrix[[0, 0, -1], [0, -1, 0], [-1, 0, 0]],
+   Matrix[[0, 0, -1], [-1, 0, 0], [0, 1, 0]],
+   Matrix[[1, 0, 0], [0, 0, -1], [0, 1, 0]],
+   Matrix[[0, -1, 0], [0, 0, -1], [1, 0, 0]],
+   Matrix[[-1, 0, 0], [0, 0, -1], [0, -1, 0]],
+   Matrix[[0, 1, 0], [0, 0, -1], [-1, 0, 0]],
+   Matrix[[-1, 0, 0], [0, 0, 1], [0, 1, 0]],
+   Matrix[[0, 1, 0], [0, 0, 1], [1, 0, 0]],
+   Matrix[[1, 0, 0], [0, 0, 1], [0, -1, 0]],
+   Matrix[[0, -1, 0], [0, 0, 1], [-1, 0, 0]]]
+end
 
-  def read_val
-    v = 0
-    while DIGITS.include? peek
-      v = v * 10 + read_char.to_i
+def cos(angle)
+  Math.cos(angle).round
+end
+
+def sin(angle)
+  Math.sin(angle).round
+end
+
+def find_if_overlap(scanner, searching)
+  get_rotations2.each_with_index do |rot, rn|
+    s1 = searching.map { |p| rot * p }
+    scanner.size.times do |i|
+      s1.size.times do |j|
+        disp = s1[j] - scanner[i]
+        matches = scanner.intersection(s1.map { |p| p - disp })
+        if matches.size >= 12
+          puts "Rot #{rn} - #{i}, #{j}: #{matches.size}"
+          # puts "Overlapping points:"
+          # puts_points(matches)
+          return [rot, disp]
+        end
+      end
     end
-    v
   end
-
-  def expect(expected)
-    c = read_char
-    throw "Exendpected '#{expected}' but was '#{c}'" if c != expected
-  end
-end
-
-class Node
-  attr_reader :l, :r
-  attr_accessor :parent
-
-  def initialize(l, r)
-    self.l = l
-    self.r = r
-  end
-
-  def l=(node)
-    @l = node
-    node.parent = self
-  end
-
-  def r=(node)
-    @r = node
-    node.parent = self
-  end
-
-  def to_s
-    "[#{l.to_s},#{r.to_s}]"
-  end
-
-  def is_leaf
-    false
-  end
-end
-
-class Leaf
-  attr_accessor :val, :parent
-
-  def initialize(val)
-    self.val = val
-  end
-
-  def to_s
-    val.to_s
-  end
-
-  def is_leaf
-    true
-  end
-end
-
-def closest_left(n)
-  while n == n.parent.l
-    n = n.parent
-    return nil if n.parent == nil
-  end
-
-  # n is now the right node of a pair
-
-  n = n.parent.l
-  while !n.is_leaf
-    n = n.r
-  end
-  n
-end
-
-def closest_right(n)
-  while n == n.parent.r
-    n = n.parent
-    return nil if n.parent == nil
-  end
-
-  # n is now the left node of a pair
-
-  n = n.parent.r
-  while !n.is_leaf
-    n = n.l
-  end
-  n
-end
-
-def explode(pair)
-  explode_n(pair, 0)
-end
-
-def explode_n(pair, depth)
-  if pair.is_leaf
-    return false
-  end
-
-  if depth > 3
-    explode_node(pair)
-    return true
-  end
-
-  return explode_n(pair.l, depth + 1) || explode_n(pair.r, depth + 1)
-end
-
-def explode_node(n)
-  cl = closest_left(n.l)
-  cl.val += n.l.val unless cl.nil?
-
-  cr = closest_right(n.r)
-  cr.val += n.r.val unless cr.nil?
-
-  if n == n.parent.l
-    n.parent.l = Leaf.new(0)
-  elsif n == n.parent.r
-    n.parent.r = Leaf.new(0)
-  else
-    throw("Orphan?")
-  end
-end
-
-def split_pair(pair)
-  if pair.is_leaf
-    if pair.val < 10
-      return false
-    else
-      split_leaf pair
-      return true
-    end
-  else
-    return split_pair(pair.l) || split_pair(pair.r)
-  end
-end
-
-def split_leaf(leaf)
-  l = leaf.val / 2
-  r = leaf.val.odd? ? (leaf.val + 1) / 2 : leaf.val / 2
-  node = Node.new(
-    Leaf.new(l),
-    Leaf.new(r)
-  )
-  if leaf.parent.l == leaf
-    leaf.parent.l = node
-  else
-    leaf.parent.r = node
-  end
-end
-
-DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-def add(pair1, pair2)
-  pair = Node.new(pair1, pair2)
-  while explode(pair) || split_pair(pair)
-  end
-  pair
-end
-
-def magnitude(pair)
-  if pair.is_leaf
-    return pair.val
-  else
-    return 3 * magnitude(pair.l) + 2 * magnitude(pair.r)
-  end
+  return nil
 end
--- main.rb
+++ main.rb
@@ -1,27 +1,39 @@
 require_relative "lib.rb"
 
-lines = $stdin.read.split("\n")
+scanners = read_scanners
+ordering = Array.new(scanners.size) { |i| i }
+# Loop precondition:
+# - everything before e has been explored for matches
+# - everything before u has found a match (with lower index) and been transformed
+# to coordinates relative to scanner[0]
+e = 0
+u = 1
+while u < scanners.size
+  (u..scanners.size - 1).each do |i|
+    puts "Looking for overlap  (#{e}, #{i})"
 
-pairs = lines.map { |l| PairParser.new(l).parse }
+    res = find_if_overlap(scanners[e], scanners[i])
 
-# puts pairs.map(&:to_s)
-result = pairs.reduce { |sum, pair|
-  add(sum, pair)
-}
+    if res
+      if e == 3 && i == 4
+        puts scanners[e], scanners[i]
+      end
+      rot, disp = res
+      scanners[i] = scanners[i].map { |p| rot * p - disp }
+      puts "Got a hit: #{disp} - putting #{i} to #{u}"
 
-puts result.to_s
-puts "Part1: magnitude: #{magnitude(result)}"
+      tmp = scanners[u]
+      scanners[u] = scanners[i]
+      scanners[i] = tmp
 
-max_mag = 0
-lines.each_with_index do |line1, a|
-  lines.each_with_index do |line2, b|
-    if (a != b)
-      pair1 = PairParser.new(line1).parse
-      pair2 = PairParser.new(line2).parse
-      pair = add(pair1, pair2)
-      mag = magnitude(pair)
-      max_mag = mag if mag > max_mag
+      tmp = ordering[u]
+      ordering[u] = ordering[i]
+      ordering[i] = tmp
+      puts "Ordering: #{ordering.to_s}"
+      u += 1
     end
   end
+  e += 1
 end
-puts "Part2 MAX: #{max_mag}"
+
+puts scanners.reduce(&:union).size
