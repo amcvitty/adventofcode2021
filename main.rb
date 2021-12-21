b1c2300b5d3e7f40a91b60c047a89a67c3e133cf
Andy McVitty <andy.mcvitty@gmail.com>
2021-12-21 23:57:21 +0000

    Day 21 Part 2

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 require_relative "lib.rb"
 
-# positions =
-puts play_game(DeterministicDice.new, [4, 8], 1000)
+# # positions =
+# puts play_game(DeterministicDice.new, [4, 8], 1000)
 
-# Part 1:
-puts play_game(DeterministicDice.new, [8, 5], 1000)
+# # Part 1:
+# puts play_game(DeterministicDice.new, [8, 5], 1000)
 
-pos = [4, 8]
-# puts play_game(DiracDice.new(1111111111), pos, 9)
+# pos = [4, 8]
+# # puts play_game(DiracDice.new(1111111111), pos, 9)
 
 # puts explore_games(0, pos, 21)
+
+puts get_wins_freq([8, 5], [0, 0], 0)
--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -3,22 +3,10 @@ require "matrix"
 require_relative "../lib.rb"
 
 RSpec.describe "code" do
-  it "dirac dice" do
-    dice = DiracDice.new(123)
-    expect(dice.roll).to eq 3
-    expect(dice.roll).to eq 2
-    expect(dice.roll).to eq 1
-    expect(dice.roll).to be_nil
+  it "adds" do
+    expect(add([1, 2], [5, 6])).to eq [6, 8]
   end
-
-  it "dirac dice" do
-    dice = DiracDice.new(1)
-    expect(play_game(dice, [4, 8], 10)).to be_nil
-  end
-
-  it "dirac dice" do
-    dice = DiracDice.new(11111111111111111111111)
-    expect(play_game(dice, [1, 2], 10)).to eq 0
-    expect(play_game(dice, [9, 2], 10)).to eq 1
+  it "multiplys" do
+    expect(multiply([1, 2], 4)).to eq [4, 8]
   end
 end
