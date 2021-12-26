8b2e1ce14ae4b8f03128a2fdb6330978edacfbd8
Andy McVitty <andy.mcvitty@gmail.com>
2021-12-26 17:03:08 +0000

    Failed attempt

--- lib.rb
+++ lib.rb
@@ -1,122 +1,126 @@
-require "matrix"
-$verbose = false
-
-List = Struct.new(:hd, :tail) do
-  def to_s
-    if tail
-      "#{hd},#{tail}"
-    else
-      hd.to_s
-    end
-  end
-
-  def ==(other)
-    if other.nil? || other.hd != hd
-      false
-    else
-      tail == other.tail
-    end
-  end
-
-  def size
-    1 + (tail.nil? ? 0 : tail.size)
-  end
-
-  def each(&block)
-    block.call(hd)
-    tail.each(&block) if tail
-  end
+struct Move(:s, :f, :path) do
 end
 
-def cons(a, as)
-  List.new(a, as)
-end
+# require "matrix"
 
-Node = Struct.new(:range, :val) do
-  def to_s
-    "#{range}:#{val}"
-  end
+# $verbose = false
 
-  def +(other)
-    cons(self, other)
-  end
-end
+# List = Struct.new(:hd, :tail) do
+#   def to_s
+#     if tail
+#       "#{hd},#{tail}"
+#     else
+#       hd.to_s
+#     end
+#   end
 
-def insert(b, list)
-  if list.nil?
-    return cons(Node.new(b.range, merge(b.val, nil)), nil)
-  end
+#   def ==(other)
+#     if other.nil? || other.hd != hd
+#       false
+#     else
+#       tail == other.tail
+#     end
+#   end
 
-  # Crucial that b's val overwrites a here
-  a = list.hd
-  ra = a.range
-  rb = b.range
+#   def size
+#     1 + (tail.nil? ? 0 : tail.size)
+#   end
 
-  nodes = [Node.new(rb.first..[ra.first - 1, rb.last].min, merge(b.val, nil)),
-           Node.new([rb.first, ra.first].max..[rb.last, ra.last].min, merge(b.val, a.val)),
-           Node.new(ra.first..[rb.first - 1, ra.last].min, a.val),
-           Node.new([ra.first, rb.last + 1].max..ra.last, a.val)]
-    .filter { |n| n.range.size > 0 }
-    .sort! { |a, b| b.range.first <=> a.range.first }
+#   def each(&block)
+#     block.call(hd)
+#     tail.each(&block) if tail
+#   end
+# end
 
-  # portion of b that comes after a - need to insert because could be more overlap in tail
-  unmerged = Node.new([rb.first, ra.last + 1].max..rb.last, b.val)
-  ret = list.tail
-  ret = insert(unmerged, list.tail) if unmerged.range.size > 0
-  nodes.each do |n|
-    ret = cons n, ret
-  end
-  ret
-end
+# def cons(a, as)
+#   List.new(a, as)
+# end
 
-# B takes precendence here
-def merge(b, a)
-  if b.is_a?(String) || b.is_a?(Integer)
-    b
-  elsif b.is_a?(Node) && (a.is_a?(List) || a.nil?)
-    insert(b, a)
-  else
-    throw "Unexpected types #{b.class} #{a.class}"
-  end
-end
+# Node = Struct.new(:range, :val) do
+#   def to_s
+#     "#{range}:#{val}"
+#   end
 
-def parse_lines(lines)
-  lines.map do |line|
-    parse_line(line)
-  end
-end
+#   def +(other)
+#     cons(self, other)
+#   end
+# end
 
-def parse_line(line)
-  d, x1, x2, y1, y2, z1, z2 = /(o\w+) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/.match(line).captures
-  x1, x2, y1, y2, z1, z2 = [x1, x2, y1, y2, z1, z2].map(&:to_i)
+# def insert(b, list)
+#   if list.nil?
+#     return cons(Node.new(b.range, merge(b.val, nil)), nil)
+#   end
 
-  Node.new(x1..x2,
-           Node.new(y1..y2,
-                    Node.new(z1..z2, d)))
-end
+#   # Crucial that b's val overwrites a here
+#   a = list.hd
+#   ra = a.range
+#   rb = b.range
 
-def limit(a, b)
-  throw "not in order" if a > b
-  if b < -50 or a > 50
-    return 1..0
-  else
-    a = a > 50 ? 50 : (a < -50 ? -50 : a)
-    b = b > 50 ? 50 : (b < -50 ? -50 : b)
-    return a..b
-  end
-end
+#   nodes = [Node.new(rb.first..[ra.first - 1, rb.last].min, merge(b.val, nil)),
+#            Node.new([rb.first, ra.first].max..[rb.last, ra.last].min, merge(b.val, a.val)),
+#            Node.new(ra.first..[rb.first - 1, ra.last].min, a.val),
+#            Node.new([ra.first, rb.last + 1].max..ra.last, a.val)]
+#     .filter { |n| n.range.size > 0 }
+#     .sort! { |a, b| b.range.first <=> a.range.first }
 
-def count(list, val)
-  sum = 0
-  list.each { |x|
-    x.val.each { |y|
-      y.val.each { |z|
-        # We now have a cuboid!
-        if z.val == val
-          sum += x.range.size * y.range.size * z.range.size
-        end
-      }
-    }
-  }
-  sum
-end
+#   # portion of b that comes after a - need to insert because could be more overlap in tail
+#   unmerged = Node.new([rb.first, ra.last + 1].max..rb.last, b.val)
+#   ret = list.tail
+#   ret = insert(unmerged, list.tail) if unmerged.range.size > 0
+#   nodes.each do |n|
+#     ret = cons n, ret
+#   end
+#   ret
+# end
+
+# # B takes precendence here
+# def merge(b, a)
+#   if b.is_a?(String) || b.is_a?(Integer)
+#     b
+#   elsif b.is_a?(Node) && (a.is_a?(List) || a.nil?)
+#     insert(b, a)
+#   else
+#     throw "Unexpected types #{b.class} #{a.class}"
+#   end
+# end
+
+# def parse_lines(lines)
+#   lines.map do |line|
+#     parse_line(line)
+#   end
+# end
+
+# def parse_line(line)
+#   d, x1, x2, y1, y2, z1, z2 = /(o\w+) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)/.match(line).captures
+#   x1, x2, y1, y2, z1, z2 = [x1, x2, y1, y2, z1, z2].map(&:to_i)
+
+#   Node.new(x1..x2,
+#            Node.new(y1..y2,
+#                     Node.new(z1..z2, d)))
+# end
+
+# def limit(a, b)
+#   throw "not in order" if a > b
+#   if b < -50 or a > 50
+#     return 1..0
+#   else
+#     a = a > 50 ? 50 : (a < -50 ? -50 : a)
+#     b = b > 50 ? 50 : (b < -50 ? -50 : b)
+#     return a..b
+#   end
+# end
+
+# def count(list, val)
+#   sum = 0
+#   list.each { |x|
+#     x.val.each { |y|
+#       y.val.each { |z|
+#         # We now have a cuboid!
+#         if z.val == val
+#           sum += x.range.size * y.range.size * z.range.size
+#         end
+#       }
+#     }
+#   }
+#   sum
+# end
--- main.rb
+++ main.rb
@@ -1,34 +1,37 @@
 require_relative "lib.rb"
-lines = $stdin.read.split "\n"
+# lines = $stdin.read.split "\n"
 
-result = lines.reduce(nil) { |grid, line|
-  node = parse_line(line)
-  puts line
-  insert(node, grid)
+singles = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+  [6, 7],
+  [8, 9],
+  [13, 14],
+]
+
+doubles = [
+  [9, 10],
+  [10, 11],
+  [11, 12],
+  [12, 13],
+]
+NODES = 15
+adj = Array.new(NODES) { Array.new(NODES, nil) }
+
+NODES.times.each { |v1|
+  NODES.times.each { |v2|
+    if singles.any? { |s|
+      (s[0] == v1 && s[1] == v2) ||
+      (s[0] == v2 && s[1] == v1)
+    }
+      x = adj[v1]
+      x[v2] = 1
+    end
+
+    if doubles.any? { |d| d[0] == v1 && d[1] == v2 || d[0] == v2 && d[1] == v1 }
+      adj[v1][v2] = 2
+    end
+  }
 }
-
-puts count(result, "on")
-
-# Example 3D with vizualization
-# list = nil
-# list = insert(Node.new(1..3, Node.new(1..9, Node.new(1..9, "."))), list)
-# list = insert(Node.new(1..3, Node.new(4..6, Node.new(4..6, "X"))), list)
-# list = insert(Node.new(2..3, Node.new(5..7, Node.new(5..7, "Z"))), list)
-# puts list
-
-# list.each { |x|
-#   x.range.each { |_row|
-#     x.val.each { |y|
-#       y.range.each { |_c|
-#         y.val.each { |z|
-#           z.range.each { |_c|
-#             print z.val
-#           }
-#         }
-#         puts
-#       }
-#     }
-#     puts "---"
-#   }
-# }
-# puts count(list, "Z")
+puts adj.map(&:to_s)
